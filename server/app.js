f2a3b22e4d93836f8b5fe52318abc6f595717c68
evankielley <evan.james.nl@gmail.com>
2017-01-29 13:21:48 -0330

    Adding mongoUtil file

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@
 let express = require('express');
 let app = express();
 
+let mongoUtil = require('./mongoUtil');
+mongoUtil.connect();
+
 app.use(express.static(__dirname + "/../client"));
 app.get("/icebergId", (request, response) => {
   response.json(["One","Two"]);
